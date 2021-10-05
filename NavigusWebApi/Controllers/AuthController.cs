4421390214885f1612d6ccd163d8a81185368f00
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-10-05 17:35:09 +0530

    Initial commit

---
+++ NavigusWebApi/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NavigusWebApi.Models;
+
+namespace NavigusWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel user)
+        {
+            return Ok(user);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# NavigusWebApi
+ 
