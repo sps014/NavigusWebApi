5d2fb31797433b5fa387c0d7e9622fd2f3675f19
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-10-07 20:11:05 +0530

    fixed supressed bug attribute

--- NavigusWebApp/Server/Program.cs
+++ NavigusWebApp/Server/Program.cs
@@ -19,7 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(x => x.SuppressModelStateInvalidFilter = true);
 
 //add firebase database
 var db = ConfigureFirebase();
