57e51216baf66ccc2fc4e94f6805176aaef66cdf
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-10-07 14:21:32 +0530

    added signup

--- NavigusWebApp/Client/Data/CredsData.cs
+++ NavigusWebApp/Client/Data/CredsData.cs
@@ -4,10 +4,9 @@ namespace NavigusWebApp.Client.Data
 {
     public static class CredsData
     {
-        public static string UID { get; set; }
         public static string JWT { get; set; }
         public static Roles Role {  get; set; }
-        public static bool IsLogin => UID != null && JWT != null;
+        public static bool IsLogin =>JWT != null;
         
     }
 }
